5580b112dfd19ddd41400b3510349f3858235d43
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-04-11 23:08:38 +0200

    Door Prefab

---
+++ Assets/Script/TeleportOnCollision.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+namespace Paws { 
+    public class TeleportOnCollision : MonoBehaviour
+    {
+        [SerializeField] private Transform _destination;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_destination) other.transform.position = _destination.position;
+        }
+
+        public void SetDestination(Transform destination)
+        {
+            _destination = destination;
+        }
+    }
+}
